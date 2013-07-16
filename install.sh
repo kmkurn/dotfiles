4b24f199f909e38a67fc79938cce770635b4f36a
Kemal Maulana <kemskems12@gmail.com>
2013-07-17 01:33:26 +0800

    Add install.sh script to automate dotfiles installation

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Rename existing file with .old extension and copy dotfiles to home dir
+use() {
+	file="$1"
+	if [ -f ~/"$file" ]; then
+		mv ~/"$file" ~/"$file".old
+	fi
+	cp "$file" ~/"$file"
+}
+
+# Use .bash_aliases
+use .bash_aliases
+
+# Copy .bash_ps1
+cp .bash_ps1 ~/.bash_ps1
+
+# Use .bashrc and apply it to current shell
+use .bashrc
+. ~/.bashrc
+
+# Copy .vimrc
+use .vimrc
